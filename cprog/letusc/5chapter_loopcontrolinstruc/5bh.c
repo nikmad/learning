8587aa72b13a27d14fbc5cd41db412e493973dd5
Nikhil Madduri <nikhil.madduri@gmail.com>
2018-10-06 11:06:43 +0530

    added 5bh.c

---
+++ cprog/letusc/5chapter_loopcontrolinstruc/5bh.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+   int p, octl=0, num, z;
+   float pow = 0.1;
+
+   printf("Enter any integer: ");
+   scanf("%d", &num);
+   z = num;
+   
+   while(z>0 || z<0)
+   {
+      p = z%8; //p = remainder
+      z = z/8; //z = quotient
+      pow *= 10;
+      octl += pow*p;
+   }
+   
+   printf("Decimal number '%d' in Octal system is '%d'.\n", num, octl);
+ 
+   return 0;
+}

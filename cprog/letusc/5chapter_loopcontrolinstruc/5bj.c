2185aa145cc1137b712b2966e8c9461d4cb728ea
Nikhil Madduri <nikhil.madduri@gmail.com>
2018-10-10 12:11:44 +0530

    added comments to 5bj.c

--- cprog/letusc/5chapter_loopcontrolinstruc/5bj.c
+++ cprog/letusc/5chapter_loopcontrolinstruc/5bj.c
@@ -23,7 +23,13 @@ int main()
    while(ch=='y')
    {
       printf("Enter 'y' if you want to enter one more number, or 'n' if you don't want to: ");
-      scanf("\n%c", &ch);
+      scanf("\n%c", &ch); 
+      
+      /* newline character in this scanf statement is intentionally inserted. Otherwise, after scanning for 'b' above, 
+      when we press enter, the scanf of 'ch' will take that previous 'Enter' as the character input for 'ch' and 
+      would not prompt you to enter y/n for 'ch'. To circumvent this issue, we use \n so that scanf will ignore one 
+      'Enter' and then ask you for an input y/n. */
+      
       if(ch=='y')
       {
          printf("Enter another number: ");
